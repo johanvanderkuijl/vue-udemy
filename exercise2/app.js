e4be321c8bd1eee7372cdc4696efd474e713d2b5
Johan van der Kuijl <johan.kuijl@alliander.com>
2019-11-22 09:15:14 +0100

    update

--- exercise2/app.js
+++ exercise2/app.js
@@ -1,6 +1,20 @@
 new Vue({
-        el: '#exercise',
-        data: {
-            value: ''
+    el: '#exercise',
+    data: {
+        keyValue: '',
+        enterValue: ''
+    },
+    methods: {
+        alertMe: function () {
+            console.log("alert");
+            alert('hi');
+        },
+        // keyDown: function (event) {
+        //     // console.log(event.target.value);
+        //     this.keyValue = event.target.value;
+        // },
+        enterDown: function (event) {
+            this.enterValue = event.target.value;
         }
-    });
+    }
+});
