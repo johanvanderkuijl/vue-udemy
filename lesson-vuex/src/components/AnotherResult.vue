f764abfc38aa0fb9d4b733a891311c3c16890ced
Johan van der Kuijl <johan.kuijl@alliander.com>
2020-01-11 11:11:22 +0100

    vuex firebase

---
+++ lesson-vuex/src/components/AnotherResult.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <p>Counter is: {{ doubleCounter }}</p>
+        <p>Number of clicks: {{ stringCounter }}</p>
+
+    </div>
+</template>
+
+<script>
+    import { mapGetters } from 'vuex';
+    import * as types from '../store/types';
+    export default {
+        // this wil map to the getter functions in the store
+        // but you cannot use your own computed properties
+        computed: {
+            ...mapGetters({
+                doubleCounter: types.DOUBLE_COUNTER,
+                stringCounter: types.CLICK_COUNTER
+            })
+        }
+        // computed: {
+        //     counter() {
+        //         return this.$store.getters.doubleCounter;
+        //     },
+        //     clicks() {
+        //         return this.$store.getters.stringCounter;
+        //     }
+        // }
+    }
+</script>
